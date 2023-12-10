eccd93223ba67967eb8809b4a98804178c9f780d
bagantegianluca <bagantegianluca@gmail.com>
2023-12-10 22:14:53 +0100

    Add loop to print all cards and their object properties

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
 Organizzare i singoli membri in card/schede */
 
-// Create an array variable to contain all team member
+// Create an objects array and set it into a variable to contain all team members
 const team = [
     {
         name: 'John',
@@ -19,13 +19,13 @@ const team = [
         pic: './img/pic1.jfif'
     },
     {
-        name: 'Jane',
-        role: 'CMO',
+        name: 'Joseph',
+        role: 'CFO',
         pic: './img/pic2.jfif'
     },
     {
-        name: 'Joseph',
-        role: 'CFO',
+        name: 'Jane',
+        role: 'CMO',
         pic: './img/pic3.jfif'
     },
     {
@@ -40,9 +40,43 @@ const team = [
     }
 ];
 
-// Create loop to print all memebers in console
+// Select row node
+const rowElement = document.querySelector('.row');
+
+// Create loop to print all memebers info in console
 for (let i = 0; i < team.length; i++) {
 
-    console.log(team[i]);
+    const member = team[i];
+    console.log(member.name, member.role, member.pic);
+
+    // Create column element
+    const colElement = document.createElement('div');
+
+    // Add column class list
+    colElement.className = 'col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3';
+
+    // Create card element
+    const cardElement = document.createElement('div');
+
+    // Add card class list
+    cardElement.className = 'card p-3';
+
+    cardChildrenElements = `
+    <img src="${member.pic}" class="card-img-top img_rounded" alt="${member.name}-pic">
+    <div class="card-body p-0">
+        <h5 class="card-title text-center">${member.name}</h5>
+        <p class="card-text">${member.name} is the <strong>${member.role}</strong> of the company</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    `
+    // Add cardChildrenElements to cardElement
+    cardElement.innerHTML = cardChildrenElements;
+
+    // Insert cardElement into colElement
+    colElement.appendChild(cardElement);
+
+    // Insert colElement into rowElement
+    rowElement.appendChild(colElement);
 
 }
+
